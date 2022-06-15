d47efd73b1efa8ecda7dc789e0e517c786f9d3bb
700grains <700grains@gmail.com>
2022-06-15 23:11:38 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args)
     {
-        int[] arr = new int[5];
-int i;
-        for (i=0; i<4; i++) {
-            System.out.println(arr[i]);
-        }
+
     }
 }
+
+
